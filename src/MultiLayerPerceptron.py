fe536bccee91468aaed999e9a7ef05d6803e1be5
pedroAcA <pedro.almeidabsb@gmail.com>
2019-05-09 01:05:15 -0300

    Adicionando plots dos padroes reconhecidos pela rede

--- src/MultiLayerPerceptron.py
+++ src/MultiLayerPerceptron.py
@@ -2,38 +2,82 @@
 
 import tensorflow as tf
 from tensorflow.keras.optimizers import SGD
-
+import numpy as np
+import matplotlib.pyplot as plt
 # Multilayer Perceptron Model
 class MLP:
-	def __init__(self, 
-				neurons_per_layer,
-				output_classes,
-				activation_functions,
-				out_layer_activation,
-				optimizer,
-				loss,
-				learning_rate):
+    def __init__(self, 
+                neurons_per_layer,
+                output_classes,
+                activation_functions,
+                out_layer_activation,
+                optimizer,
+                loss,
+                learning_rate):
 
-		# Feed-Forward Neural Network Model
-		self.model = tf.keras.models.Sequential()
+        # Feed-Forward Neural Network Model
+        self.model = tf.keras.models.Sequential()
 
-		# Flatten the 28x28 images provided
-		self.model.add(tf.keras.layers.Flatten())
+        # Flatten the 28x28 images provided
+        self.model.add(tf.keras.layers.Flatten())
 
-		# Hidden Layers
-		for neurons, act in zip(neurons_per_layer, activation_functions):
-			self.model.add(tf.keras.layers.Dense(neurons, activation=act))
+        # Hidden Layers
+        for neurons, act in zip(neurons_per_layer, activation_functions):
+            self.model.add(tf.keras.layers.Dense(neurons, activation=act))
 
-		# Output Layer
-		self.model.add(tf.keras.layers.Dense(output_classes,
-			activation=out_layer_activation))
+        # Output Layer
+        self.model.add(tf.keras.layers.Dense(output_classes,
+            activation=out_layer_activation))
 
         # Build Model
-		self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
     
-	def learn(self, X, y, epochs):
-		self.model.fit(X, y, epochs=epochs)
+    def learn(self, X, y, epochs):
+        self.model.fit(X, y, epochs=epochs)
 
-	def predict(self, X_new, batch_size=100):
-		predictions = self.model.predict(X_new, batch_size=batch_size)
-		return predictions
+    def predict(self, X_new, batch_size=100):
+        predictions = self.model.predict(X_new, batch_size=batch_size)
+        return predictions
+
+        #layer, neuron integers from 0...N-1
+    def get_weigths_from_neuron_at_layer(self, neuron, layer):
+        weights = []
+        for W_array in self.model.layers[layer].get_weights()[0]: #0 = weights, 1 = bias
+            weights.append(W_array[neuron])
+        return(np.array(weights))
+
+    def get_binary_matrix_from_neuron(self, neuron):
+        weights = self.get_weigths_from_neuron_at_layer(neuron, -2) # hidden layer closer to output layer
+        weights[weights<0] = 0 # white
+        #weights[weights>0] = 1 # black
+        weights = weights/np.amax(weights) # values between 0 and 1
+        img = weights.reshape((28, 28))
+        return img
+
+    def plot_first_layer_weight_matrix_from_neuron(self, neuron, label='', new_figure=False, block=True):
+        img = self.get_binary_matrix_from_neuron(neuron)
+        if(new_figure):
+            plt.figure() # new figure to accomodate every loop image 
+        plt.imshow(img, cmap="Greys")
+        plt.title(label)
+        plt.show(block = block)
+
+    def get_pattern_to_classify_as(self, img_class):
+        output_layer_w = self.get_weigths_from_neuron_at_layer(neuron=img_class, layer=-1)# output layer
+        sorted_indices = np.argsort(output_layer_w)[::-1] # descending order
+        pattern_recognized = np.zeros((28,28)) # sum of positive weights 
+        pattern_rejected = np.zeros((28,28)) # sum of negative weights
+        total_pattern = np.zeros((28,28)) # sum of all weights (recognized - rejected)
+        for i in sorted_indices:
+            np.add(total_pattern, output_layer_w[i]*self.get_binary_matrix_from_neuron(i), out=total_pattern)
+            if(output_layer_w[i]>0): # this pattern is accepted as to activate output from this class 
+                img = output_layer_w[i]*self.get_binary_matrix_from_neuron(i)
+                np.add(pattern_recognized, img, out=pattern_recognized)
+            else: # this pattern is rejected as activation output from this class
+                img = -output_layer_w[i]*self.get_binary_matrix_from_neuron(i)
+                np.add(pattern_rejected, img, out=pattern_rejected)
+        pattern_recognized /= np.amax(pattern_recognized)
+        pattern_rejected /= np.amax(pattern_rejected)
+        total_pattern/=np.amax(total_pattern)
+        total_pattern[total_pattern<0] = 0
+        return(pattern_recognized, pattern_rejected, total_pattern)
--- src/main.py
+++ src/main.py
@@ -5,105 +5,132 @@ from MultiLayerPerceptron import MLP
 from CustomFunctions import *
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from tensorflow.keras.optimizers import SGD
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# Function to plot an image
+def show_image(img, label,new_figure=False,block=True):
+    if(new_figure):
+        plt.figure() # new figure to accomodate every loop image 
+    plt.imshow(img, cmap="Greys")
+    plt.title(label)
+    plt.show(block = block)
 
 def main():
 
-	# Load the dataset
-	(X_train, y_train), (X_test, y_test) = load_dataset()
+    # Load the dataset
+    (X_train, y_train), (X_test, y_test) = load_dataset()
 
-	# Normalizes the dataset
-	X_train = normalize_dataset(X_train)
-	X_test 	= normalize_dataset(X_test)
+    # Normalizes the dataset
+    X_train = normalize_dataset(X_train)
+    X_test  = normalize_dataset(X_test)
 
-	# Create the Neural Network Structure
+    # Create the Neural Network Structure
 
-	# 1. Define the number of Neurons and Layers
-	neurons_per_layer = [128, 128] # 2 layers, 128 neurons each
+    # 1. Define the number of Hidden Layers and number of Neurons at each layer 
+    neurons_per_layer = [20] # 1 layer, 20 neurons
 
-	# 2. Define the number of output classes
-	output_classes = 10
+    # 2. Define the number of output classes
+    output_classes = 10
 
-	# 3. Define the activation function on hidden layers
-	# Options:
-	#   - 'relu'
-	#   - 'tanh'
-	#   - 'sigmoid'
-	activation_functions = ['relu', 'tanh']
+    # 3. Define the activation function on hidden layers
+    # Options:
+    #   - 'relu'
+    #   - 'tanh'
+    #   - 'sigmoid'
+    activation_functions = ['sigmoid']
 
-	# 4. Define the activation function on output layer
-	# Options:
-	#   - 'softmax'
-	out_layer_activation = 'softmax'
+    # 4. Define the activation function on output layer
+    # Options:
+    #   - 'softmax'
+    #   - 'sigmoid'
+    out_layer_activation = 'softmax'
 
-	# 5. Define the optimizer
-	#  Options:
-	#   - 'adam'
-	#   - SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
-	optimizer = 'sgd'
+    # 5. Define the learning rate
+    #   - Variate the learning rates!
+    learning_rate = 0.01
 
-	# 6. Define the loss function
-	# Options: 
-	#   - 'sparse_categorical_crossentropy'
-	#   - 'mean_squared_error'
-	#   -  sqr_error
-	loss = 'sparse_categorical_crossentropy'
+    # 6. Define the optimizer
+    #  Options:
+    #   - 'adam'
+    #   - SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
+    optimizer = SGD(lr=learning_rate, momentum=0.0, decay=0.0, nesterov=False)
 
-	# 7. Define the learning rate
-	#   - Variate the learning rates!
-	learning_rate = 0.01
+    # 7. Define the loss function
+    # Options: 
+    #   - 'categorical_crossentropy'
+    #   - 'mean_squared_error'
+    #   -  sqr_error
+    loss = 'categorical_crossentropy'
 
-	# Learn and Predict Model
 
-	# 1. Construct the MLP
-	mlp = MLP(neurons_per_layer,
-			  output_classes,
-			  activation_functions,
-			  out_layer_activation,
-			  optimizer,
-			  loss,
-			  learning_rate)
 
-	# 2. Fit the model
-	mlp.learn(X_train, y_train, epochs=5)
+    # Learn and Predict Model
 
-	# 3. Evaluate the model
-	test_classes = mlp.model.predict_classes(X_test)
-	test_outputs = mlp.predict(X_test)
+    # 1. Construct the MLP
+    mlp = MLP(neurons_per_layer,
+              output_classes,
+              activation_functions,
+              out_layer_activation,
+              optimizer,
+              loss,
+              learning_rate)
 
-	print('Accuracy and CM on Training Set:')
-	print(accuracy_score(y_train, mlp.model.predict_classes(X_train)))
-	print(confusion_matrix(y_train, mlp.model.predict_classes(X_train)))
+    # 2. Fit the model
+    mlp.learn(X_train, to_categorical(y_train), epochs=5)
+    # 3. Show recognized patterns
+    for output_class in range(10):
+        pattern_recognized, pattern_rejected, total_pattern = mlp.get_pattern_to_classify_as(output_class)
+        show_image(pattern_recognized, label = 'pattern recognized for class {}'.format(output_class),
+                   new_figure=True, block=False)
+        #show_image(pattern_rejected, label = 'pattern rejected for class {}'.format(output_class),
+                   #new_figure=True, block=False)
+        show_image(total_pattern, label = 'Remainder pattern for class {}'.format(output_class),
+                   new_figure=True, block=False)
+    plt.show()
 
-	print('Accuracy and CM on Test Set:')
-	print(accuracy_score(y_test, mlp.model.predict_classes(X_test)))
-	print(confusion_matrix(y_test, mlp.model.predict_classes(X_test)))
+    # 4. Evaluate the model
+    test_classes = mlp.model.predict_classes(X_test)
+    test_outputs = mlp.predict(X_test)
 
-	# Previous attempt
+    print('Accuracy and CM on Training Set:')
+    print(accuracy_score(y_train, mlp.model.predict_classes(X_train)))
+    print(confusion_matrix(y_train, mlp.model.predict_classes(X_train)))
 
-	# mlp = MLP([60, 10], 0.01)
-	# print('non categorical shape and first sample')
-	# print(y_train.shape)
-	# print(y_train[0])
-	# print('categorical shape and first sample')
-	# print(to_categorical(y_train).shape)
-	# print(to_categorical(y_train)[0])
+    print('Accuracy and CM on Test Set:')
+    print(accuracy_score(y_test, mlp.model.predict_classes(X_test)))
+    print(confusion_matrix(y_test, mlp.model.predict_classes(X_test)))
 
-	# # Converting y_train to categorical will transform the outputs
-	# # as a one hot variable (1 for the desired class and 0 for the others)
-	# # allowing the network to train each output neuron.
-	
-	# mlp.learn(x_train, to_categorical(y_train), epochs=10)
-	# test_classes  = mlp.model.predict_classes(x_test)
-	# test_outputs = mlp.predict(x_test)
+    # Previous attempt
+
+    # mlp = MLP([60, 10], 0.01)
+    # print('non categorical shape and first sample')
+    # print(y_train.shape)
+    # print(y_train[0])
+    # print('categorical shape and first sample')
+    # print(to_categorical(y_train).shape)
+    # print(to_categorical(y_train)[0])
+
+    # # Converting y_train to categorical will transform the outputs
+    # # as a one hot variable (1 for the desired class and 0 for the others)
+    # # allowing the network to train each output neuron.
+    
+    # mlp.learn(x_train, to_categorical(y_train), epochs=10)
+    # test_classes  = mlp.model.predict_classes(x_test)
+    # test_outputs = mlp.predict(x_test)
 
 
 def load_dataset():
-	mnist = tf.keras.datasets.mnist
-	return mnist.load_data()
+    mnist = tf.keras.datasets.mnist
+    return mnist.load_data()
 
 def normalize_dataset(X):
-	return tf.keras.utils.normalize(X)
+    return tf.keras.utils.normalize(X)
 
 
 if __name__ == "__main__":
-	main()
+    main()
